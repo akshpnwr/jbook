763cbd68f191a38031e639913fceb9bec5e45bd5
akshpnwr <akashpanwar9976@gmail.com>
2024-01-21 15:10:09 +0530

    Initial Setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState } from "react";
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    const [inputText, setInputText] = useState<string>("");
+
+    const onSubmit = () => {
+        console.log("onSubmit");
+        console.log(inputText);
+    }
+    
+    return <div>
+        <textarea name="" id="" cols={30} rows={10} onChange={(e)=>setInputText(e.target.value)}></textarea>
+        <br />
+        <button type="submit" onClick={onSubmit}>Submit</button>
+    </div>;
+}
+
+createRoot(document.getElementById("root") as HTMLElement).render(<App />);
