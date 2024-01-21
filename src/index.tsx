fd463fa39114dd48f5246d6c47358b01e1c92430
akshpnwr <akashpanwar9976@gmail.com>
2024-01-21 15:57:43 +0530

    Transpiling code using esbuild-wasm

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,50 @@
-import { useState } from "react";
-import { createRoot } from 'react-dom/client';
+import * as esbuild from "esbuild-wasm";
+import { useEffect, useRef, useState } from "react";
+import { createRoot } from "react-dom/client";
 
 const App = () => {
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+  const ref = useRef<any>();
 
-    const [inputText, setInputText] = useState<string>("");
+  useEffect(() => {
+    startService();
+  }, []);
 
-    const onSubmit = () => {
-        console.log("onSubmit");
-        console.log(inputText);
-    }
-    
-    return <div>
-        <textarea name="" id="" cols={30} rows={10} onChange={(e)=>setInputText(e.target.value)}></textarea>
-        <br />
-        <button type="submit" onClick={onSubmit}>Submit</button>
-    </div>;
-}
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "./esbuild.wasm",
+    });
+  };
+
+  const onClick = async () => {
+    if (!ref.current) return;
+
+    const res = await ref.current.transform(input, {
+      loader: "jsx",
+      target: "es2015",
+    });
+
+    setCode(res.code);
+  };
+
+  return (
+    <div>
+      <textarea
+        name=""
+        id=""
+        cols={30}
+        rows={10}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <br />
+      <button type="submit" onClick={onClick}>
+        Submit
+      </button>
+      <pre>{code}</pre>
+    </div>
+  );
+};
 
 createRoot(document.getElementById("root") as HTMLElement).render(<App />);
