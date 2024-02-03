24ec721568d89c0ce7bd9723a1728f766c05d5cf
akshpnwr <akashpanwar9976@gmail.com>
2024-02-03 11:53:58 +0530

    Added the action bar component

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,4 +1,6 @@
+import './cell-list-item.css'
 import { Cell } from '../state'
+import ActionBar from './action-bar'
 import CodeCell from './code-cell'
 import TextEditor from './text-editor'
 
@@ -7,12 +9,26 @@ interface CellListItemProps {
 }
 
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
-  return (
-    <div>
-      {cell.type === 'code' && <CodeCell cell={cell} />}
-      {cell.type === 'text' && <TextEditor cell={cell} />}
-    </div>
-  )
+  let child: JSX.Element
+
+  if (cell.type === 'code') {
+    child = (
+      <>
+        <div className="action-bar-wrapper">
+          <ActionBar id={cell.id} />
+        </div>
+        <CodeCell cell={cell} />
+      </>
+    )
+  } else {
+    child = (
+      <>
+        <TextEditor cell={cell} />
+        <ActionBar id={cell.id} />
+      </>
+    )
+  }
+  return <div className="cell-list-item">{child}</div>
 }
 
 export default CellListItem
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './state'
 import CellList from './components/cell-list'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 const App = () => {
   return (
